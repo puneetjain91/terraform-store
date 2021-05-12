6588b37a64b6e6593dd973798ec17f340329cf89
puneetjain91 <40116199+puneetjain91@users.noreply.github.com>
2021-05-12 16:01:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,8 @@ resource "google_container_cluster" "primary" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
